45b5612715196ac98bf11b1f08d0f3b8e0577e4a
Michael Durso Jr <rbprogrammer@gmail.com>
2018-05-20 17:54:20 +0100

    Removing initial skeleton code.

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+// todo docs
+module.exports = {
 };
